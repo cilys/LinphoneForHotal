e5f51cb10e5f108bd56c431becf906c7507ed878
cilys <1321851830@qq.com>
2021-02-04 12:50:54 +0800

    主页轮播

---
+++ app/src/main/java/com/cilys/linphoneforhotal/home/BannerInfo.java
@@ -0,0 +1,15 @@
+package com.cilys.linphoneforhotal.home;
+
+import com.stx.xhb.xbanner.entity.BaseBannerInfo;
+
+public class BannerInfo implements BaseBannerInfo {
+    @Override
+    public Object getXBannerUrl() {
+        return null;
+    }
+
+    @Override
+    public String getXBannerTitle() {
+        return null;
+    }
+}
