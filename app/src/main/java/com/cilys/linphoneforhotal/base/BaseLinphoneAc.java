aa2b33223f1fb832dfa8c193f148ad19cbcf7a81
cilys <1321851830@qq.com>
2021-02-05 01:30:54 +0800

    debug变量

--- app/src/main/java/com/cilys/linphoneforhotal/base/BaseLinphoneAc.java
+++ app/src/main/java/com/cilys/linphoneforhotal/base/BaseLinphoneAc.java
@@ -39,7 +39,7 @@ public abstract class BaseLinphoneAc extends BaseAc {
 
 
 
-        if (BuildConfig.DEBUG || true) {
+        if (BuildConfig.DEBUG) {
             View decorView = getWindow().getDecorView();
             int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
