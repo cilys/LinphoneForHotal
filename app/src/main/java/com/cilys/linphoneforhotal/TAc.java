0868d9626dbf51450903df87ba5f463a62166930
cilys <1321851830@qq.com>
2021-02-28 21:42:03 +0800

    service menu

---
+++ app/src/main/java/com/cilys/linphoneforhotal/TAc.java
@@ -0,0 +1,13 @@
+package com.cilys.linphoneforhotal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TAc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_t);
+    }
+}
